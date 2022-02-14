f9767cb2f13e0dfdac1c182efcb0c56a83679a30
Ricardo Biruel <biruel@infodesk.com.br>
2022-02-14 20:38:21 -0300

    fix: yarn deploy

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,17 @@
-export declare const generaToken: () => string;
-export declare const isObject: (obj: any) => boolean;
-export declare const capitalize: (str: string) => string;
-export declare const returnOnlyNumbers: (str: string) => string;
-export declare const clone: (obj: Object) => Object;
 export declare const ROOT_PATH: string;
+export declare const clone: (obj: Object) => Object;
+export declare const isObject: (obj: any) => boolean;
+export declare const returnOnlyNumbers: (str: string) => string;
+export declare const capitalize: (str: string) => string;
+export declare const generaToken: () => string;
+export declare const returnPhoneMask: (str: string, countryCode?: string) => string | undefined;
 declare const index: {
     ROOT_PATH: string;
     clone: (obj: Object) => Object;
+    isObject: (obj: any) => boolean;
     returnOnlyNumbers: (str: string) => string;
     capitalize: (str: string) => string;
-    isObject: (obj: any) => boolean;
     generaToken: () => string;
+    returnPhoneMask: (str: string, countryCode?: string) => string | undefined;
 };
 export default index;
