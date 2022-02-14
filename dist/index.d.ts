20b1d91440dc8b2a19037d80ae078dda504d3e1a
Ricardo Biruel <biruel@infodesk.com.br>
2022-02-14 13:14:08 -0300

    fix: added the dist/ folder to package editing the .ignore file

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+export declare const generaToken: () => string;
+export declare const isObject: (obj: any) => boolean;
+export declare const capitalize: (str: string) => string;
+export declare const returnOnlyNumbers: (str: string) => string;
+export declare const clone: (obj: Object) => Object;
+export declare const ROOT_PATH: string;
+declare const index: {
+    ROOT_PATH: string;
+    clone: (obj: Object) => Object;
+    returnOnlyNumbers: (str: string) => string;
+    capitalize: (str: string) => string;
+    isObject: (obj: any) => boolean;
+    generaToken: () => string;
+};
+export default index;
+//# sourceMappingURL=index.d.ts.map
