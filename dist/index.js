200628cb0a804e0b2d8d1e599035db2c813458d3
Ricardo Biruel <biruel@infodesk.com.br>
2022-02-14 20:25:04 -0300

    fix: dist/index.js with new method 'returnPhoneMask()'

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROOT_PATH = exports.clone = exports.returnOnlyNumbers = exports.capitalize = exports.isObject = exports.generaToken = void 0;
+exports.returnPhoneMask = exports.generaToken = exports.capitalize = exports.returnOnlyNumbers = exports.isObject = exports.clone = exports.ROOT_PATH = void 0;
 const crypto_1 = require("crypto");
 const path_1 = __importDefault(require("path"));
 const capitalize_pt_br_1 = __importDefault(require("capitalize-pt-br"));
 // ************************************************************************** //
-// generaToken()
-// generate a randon token using the crypto package
+// ROOTH_PATH
+// constant with the NodeJS project root path (ex: /Users/biruel/Dropbox/dev/jarvisly-v2/code-backend)
 // ************************************************************************** //
-const generaToken = () => (0, crypto_1.randomUUID)();
-exports.generaToken = generaToken;
+const mainFile = (require.main && path_1.default.dirname(require.main.filename)) || '.'; // get the main file (ex: app.js) path
+exports.ROOT_PATH = path_1.default.join(mainFile, '..'); // Root Path to the main file
+// ************************************************************************** //
+// clone()
+// clone object broken all deep references
+// ************************************************************************** //
+const clone = (obj) => {
+    return JSON.parse(JSON.stringify(obj));
+};
+exports.clone = clone;
 // ************************************************************************** //
 // isObject()
 // check if the obj is really a valid javascript object (POJO)
@@ -23,41 +31,60 @@ const isObject = (obj) => {
 exports.isObject = isObject;
 // ************************************************************************** //
 // returnOnlyNumbers()
-// words capitalize
-// ************************************************************************** //
-const capitalize = (str) => (0, capitalize_pt_br_1.default)(str);
-exports.capitalize = capitalize;
-// ************************************************************************** //
-// returnOnlyNumbers()
 // return only numbers from a string
 // ************************************************************************** //
 const returnOnlyNumbers = (str) => str.replace(/\D/g, '');
 exports.returnOnlyNumbers = returnOnlyNumbers;
 // ************************************************************************** //
-// clone()
-// clone object broken all deep references
+// returnOnlyNumbers()
+// return string with words capitalize ignoreing the prepositions
 // ************************************************************************** //
-const clone = (obj) => {
-    return JSON.parse(JSON.stringify(obj));
+const capitalize = (str) => (0, capitalize_pt_br_1.default)(str);
+exports.capitalize = capitalize;
+// ************************************************************************** //
+// generaToken()
+// generate a random token using the node crypto core package
+// ************************************************************************** //
+const generaToken = () => (0, crypto_1.randomUUID)();
+exports.generaToken = generaToken;
+// ************************************************************************** //
+// returnPhoneMask()
+// Return the phone number mask according the contry and type of phone (defined by number length)
+// ************************************************************************** //
+const returnPhoneMask = (str, countryCode = 'br') => {
+    if (!str)
+        return;
+    if (countryCode === 'br') {
+        if (str.length === 11) {
+            if (str.substring(0, 1) === '0') { // 0800-PJMONEY
+                return '#### ## ####';
+            }
+            else {
+                return '(##) #.####-####';
+            }
+        }
+        else if (str.length === 10) {
+            return '(##) ####-####';
+        }
+        else {
+            return str;
+        }
+    }
+    else {
+        return str;
+    }
 };
-exports.clone = clone;
-// ************************************************************************** //
-// ROOTH_PATH
-// return the main path project (ex: /src)
-// ************************************************************************** //
-// get the main file (ex: app.js) path
-const mainFile = (require.main && path_1.default.dirname(require.main.filename)) || '.';
-// Root Path to the main file
-exports.ROOT_PATH = path_1.default.join(mainFile, '..');
+exports.returnPhoneMask = returnPhoneMask;
 // ========================================================================== //
 // api methods export
 // ========================================================================== //
 const index = {
     ROOT_PATH: exports.ROOT_PATH,
     clone: exports.clone,
+    isObject: exports.isObject,
     returnOnlyNumbers: exports.returnOnlyNumbers,
     capitalize: exports.capitalize,
-    isObject: exports.isObject,
     generaToken: exports.generaToken,
+    returnPhoneMask: exports.returnPhoneMask
 };
 exports.default = index;
